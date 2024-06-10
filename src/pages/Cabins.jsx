dd348dcdd942fb9d80e090784f045749f7955c56
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-06-10 13:54:55 +0600

    Introducing Another Library: React Hook Form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,10 +4,14 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import { useState } from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 function Cabins() {
   // useEffect(() => {
   //   getCabins().then((data) => console.log(data));
   // }, []);
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <>
@@ -17,6 +21,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new Cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
