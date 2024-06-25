755e4377ecfd4ffcf5a0477f822c24ea4a4af0b1
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-06-25 17:28:21 +0600

    api side sorting : sorting bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -8,10 +8,14 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, status, cabins(name), guests(fullName, email)"
     );
   //Filter
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter) query = query.eq(filter.field, filter.value);
   // if (filter !== null)
   //   query = query[filter.method || "eq"](filter.field, filter.value);
 
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
   const { data, error } = await query;
   if (error) {
     console.error(error);
