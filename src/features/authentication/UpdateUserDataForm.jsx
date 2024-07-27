c729f2af3c561f993b0f401860e69697b98244c6
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-07-27 19:34:02 +0600

    Implementing dark mode with css

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -7,6 +7,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -20,8 +21,24 @@ function UpdateUserDataForm() {
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
+  const { updateUser, isUpdating } = useUpdateUser();
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -33,6 +50,7 @@ function UpdateUserDataForm() {
         <Input
           type="text"
           value={fullName}
+          disabled={isUpdating}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
         />
@@ -41,11 +59,12 @@ function UpdateUserDataForm() {
         <FileInput
           id="avatar"
           accept="image/*"
+          disabled={isUpdating}
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button type="reset" variation="secondary" onClick={handleCancel}>
           Cancel
         </Button>
         <Button>Update account</Button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
-      <CabinTable />
+      {/* <CabinTable /> */}
     </LoginLayout>
   );
 }
