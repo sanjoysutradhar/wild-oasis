7b51bdb2cdabff78d19d4b70d8df6e83cf0a1e03
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-08-13 16:27:24 +0600

    displaying a pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
 import SalseChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,7 +38,8 @@ function DashboardLayout() {
       />
       {/* <div>Statistics</div> */}
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
+      {/* <div>Chart stay durations</div> */}
+      <DurationChart confirmedStays={confirmedStays} />
       {/* <div>Chart sales</div> */}
       <SalseChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
